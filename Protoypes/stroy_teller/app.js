7c535ff3550630ede18d281163d78c22bf8f55fa
Quay Robinson <93289432+QTEA02@users.noreply.github.com>
2023-11-08 11:18:19 -0500

    Update app.js

--- Protoypes/stroy_teller/app.js
+++ Protoypes/stroy_teller/app.js
@@ -16,11 +16,17 @@ class TreeNode {
 }
 
 const storyNodes = {
-    start: new TreeNode("Once upon a time, in a faraway land, there was a brave knight named Sir Lancelot. Sir Lancelot was on a quest to find the legendary sword Excalibur. As he ventured through the enchanted forest, he encountered a wise old wizard who offered him guidance. The wizard told him that Excalibur was hidden in a mystical lake guarded by a fearsome dragon. Will Sir Lancelot continue his quest to find Excalibur?", 'continueQuest', 'endRejected'),
+    start: new TreeNode("Once upon a time, in a faraway land, there was a brave knight named Sir Lancelot. Sir Lancelot was on a quest to find the legendary sword Excalibur. Through his travels, he encounters a wise old wizard who offers him guidance. The wizard told him that Excalibur was hidden in a mystical lake guarded by a fearsome dragon. Will Sir Lancelot continue his quest to find Excalibur?", 'continueQuest', 'endRejected'),
+    endRejected: new TreeNode("Sir Lancelot rejected the wizard's guidance. The end."),
     continueQuest: new TreeNode("Sir Lancelot accepted the wizard's guidance. Will he enter the enchanted forest?", 'enterForest', 'notEnterForest'),
     enterForest: new TreeNode("Sir Lancelot enters the enchanted forest. Will he confront the fearsome dragon?", 'confrontDragon', 'notConfrontDragon'),
-    notEnterForest: new TreeNode("Sir Lancelot decides not to enter the forest. The end."),
-    endRejected: new TreeNode("Sir Lancelot rejected the wizard's guidance. The end."),
+    notEnterForest: new TreeNode("Sir Lancelot decides not to enter the forest. Should he find another way?", 'goDifferentWay', 'doNotGoAnotherWay' ),
+    doNotGoAnotherWay: new TreeNode("Sir Lancelot chooses not go another way. You head back. The end."),
+    goDifferentWay: new TreeNode("Sir Lancelot chooses to go around and over the mountain. He encounters a troll. Will he confront the troll?", 'confrontTroll', 'notConfrontTroll' ),
+    notConfrontTroll: new TreeNode("Sir Lancelot chose not to confront the troll and go back. The end."),
+    confrontTroll: new TreeNode("Sir Lancelot chose to confront the troll. He drops something. Pick it up?" , 'pickItUP', 'notPickItUp'),
+    pickItUp: new TreeNode("Sir Lancelot pick sit up. It was the Exalibur! Congratulations. The end."),
+    notPickItUp: new TreeNode("Sir Lancelot does not pick it up. There's nothing else you can do. The end."),
     notConfrontDragon: new TreeNode("Sir Lancelot chooses not to confront the dragon. The end."),
     confrontDragon: new TreeNode("Sir Lancelot confronts the fearsome dragon. Will he succeed in retrieving Excalibur?", 'retrieveExcalibur', 'failRetrieveExcalibur'),
     retrieveExcalibur: new TreeNode("Sir Lancelot successfully retrieves Excalibur. The end."),

c59f1ab7d1e2ab31f148203b1050f5fc117ad15b
Colevy Collins <77639915+Colevy-Collins@users.noreply.github.com>
2023-12-04 10:47:45 +0000

    ai ver2

--- Protoypes/story_teller_AIver2/views/story_teller_AI/app.js
+++ Protoypes/story_teller_AIver2/views/story_teller_AI/app.js
@@ -13,12 +13,9 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 let thread
-let selectedOptions = {};
+
 let answersList = []; // Initialize the list with the start node
 let isTraverseStoryRunning = false;
-let isCreatingAI = false;
-let assistant_id = "asst_AmjQ55bLUIHbDS8BVV7zjedd";
-
 async function traverseStory(answer) {
 
     if (isTraverseStoryRunning) {
@@ -37,7 +34,7 @@ async function traverseStory(answer) {
     
         const run = await client.beta.threads.runs.create(
             thread.id,
-            { assistant_id: assistant_id}
+            { assistant_id: "asst_AmjQ55bLUIHbDS8BVV7zjedd"}
         );
     
         while (true) {
@@ -62,49 +59,6 @@ async function traverseStory(answer) {
     } finally {
         isTraverseStoryRunning = false; // Reset the flag when done
     }
-}
-
-async function createAI() {
-
-    if (isCreatingAI) {
-        console.log("createAI is already running...");
-        return; // If traverseStory is running, we simply return
-    }
-
-    isCreatingAI = true; // Set the flag to indicate traverseStory is running
-    try {
-        const assistant = await openai.beta.assistants.create({
-            name: "Story Teller V2",
-            instructions: `
-            Pretend that you are game master and create a story using the following criteria:
-            1. Begin the narrative with an introductory segment, providing background information and setting details.
-            2. Present only one prompt and get user input before the next prompt. The story should build off of the users choices.
-            3. Each prompt should offer only ${selectedOptions[0]} options for advancing the story.
-            4. When presenting the options to the user you should prefix each option with "option" and a number that correspond with that option
-            5. Craft the story around a journey involving decisions such as choosing paths, encountering characters, and engaging in combat.
-            6. Develop combat sequences with multiple prompts, detailing each step and allowing the user to choose actions and employ strategic maneuvers.
-            7. Ensure that each combat scenario has the potential to result in lasting effects, including the possibility of death. Give the user the status of their effects with each combat prompt 
-            8. Introduce the element of chance for death in every prompt. Death of the user should be very possible throughout the story. The user should be able to die for being risky or choosing a path that leads to death.
-            9. Make it clear that the story concludes if the user dies.
-            10. Establish the overarching plot of the story, focusing on the quest to locate an essential object required to confront a boss enemy.
-            11. Conclude the narrative when the boss is successfully defeated.
-            12. The story should be ${selectedOptions[1]}-themed
-            13. combat should have great detail and abundant. 
-            14. The user should be able to go back one prompt at a time to try the other option.
-            15. provide output only in a json object with the attributes for the prompt which holds all the text of the story, each option and colors that has colors for html attributes. There should be 6 colors for the web page that are vibrant and ${selectedOptions[1]} themed. The json object should have a "prompt", "optionX" where X is the number for the option, "color1", "color1", "color2", "color3", "color4", "color5", "color6".
-            16. Each prompt should contain rich details but is contained in ${selectedOptions[2]} paragraphs  
-            `,
-            model: "gpt-4-1106-preview",
-          });
-
-        assistant_id = assistant.id;
-
-    } catch (error) {
-        console.error("An error occurred during creatingAI: ", error);
-    } finally {
-        isCreatingAI = false; // Reset the flag when done
-    }
-
 }
 
 // New function to go back to the previous prompt
@@ -135,20 +89,7 @@ app.get('/currentPrompt', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.render('customize_story'); // Include answersList in the render context
-});
-
-app.post('/api/submit-form', async(req, res) => {
-    // Extract selector values
-    for (let i = 1; i <= 14; i++) {
-        const selectorKey = `selector${i}`;
-        selectedOptions[selectorKey] = req.body[selectorKey];
-    }
-
-    await createAI()
-    res.render('currentPrompt', { answersList }); // Ensure answersList is passed here
-    
-
+    res.render('currentPrompt', { answersList }); // Include answersList in the render context
 });
 
 app.get('/answer/:answer',async (req, res) => {
